eeb5a43177dd419fe6f5acb321f2b162f6676116
ksbnz <premmmhatre@gmail.com>
2026-07-21 05:19:40 +0000

    Addition

---
+++ Addition.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "\nAddition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+
+    if (b != 0)
+    {
+        cout << "Division = " << a / b << endl;
+        cout << "Modulus = " << a % b << endl;
+    }
+    else
+    {
+        cout << "Division and Modulus are not possible because the second number is 0." << endl;
+    }
+
+    return 0;
+}
