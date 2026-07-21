b29241f1608a34d2f52bd7887749c6ac31184b3e
ksbnz <premmmhatre@gmail.com>
2026-07-21 09:46:36 +0000

    addition

---
+++ file1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+namespace sycs4
+{
+    int add()
+    {
+        int a,b,sum;
+        std::cout<<"Enter two nuumbers";
+        std::cin>>a>>b;
+        std::cout<<"sum of the two numbers is"<<(a+b);
+        return 0;
+    }
+
+}
