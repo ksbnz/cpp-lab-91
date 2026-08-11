a1b2b33ab7d7eb13977858e8a49056955f9df3df
ksbnz <premmmhatre@gmail.com>
2026-08-11 10:10:51 +0000

    Area using Functions

---
+++ Functions.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int area (int);
+int area (int,int);
+float area (float);
+
+int main()
+{
+    cout<<"Calling the area() function for computing the area of a square 
+    (side =5) - " <<area(5)<<"\n";
+
+    cout<<"Calling the area() function for computing the area of a  rectangle
+     (length = 5, breadth = 10)  - "<<area(5,10)<<"\n";
+
+    cout<<"Calling the area() function for computing the area of a circle
+     (radius = 5.5) - "<<area(5.5) <<"\n";
+return 0;
+}
+ int area (int size)
+ {
+    return(size*size);
+ }
+     int area (int length, int breadth)
+ {
+    return(length*breadth);
+ }
+    float area (float radius)
+ {           
+    return (3.14*radius*radius);
+ }
+    
